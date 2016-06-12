9130d35e327514aa466c131e4fa848fa4518971c
Javaliant <luigipvincent@gmail.com>
2016-06-11 20:44:59 -0400

    Second CleanUpWords C Solution Refactor

--- CodeEval/SanitizeWords/C/Solution.c
+++ CodeEval/SanitizeWords/C/Solution.c
@@ -4,45 +4,41 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
 #include <ctype.h>
 
-#define LINE_MEMORY 1024
+#define LINE_BUFFER 512
 
-void to_lowercase(char * input) {
-	for(int i = 0; input[i]; i++){
-  		input[i] = tolower(input[i]);
-	}
-}
-
-void sanitize(char * input, char *destination, int destinationSize) {
+void sanitize(char * input) {
 	int iterator = 0;
-	int character_value;
 	bool match_found = false;
+	bool space_needed = false;
 
-	for (int i = 0; i < strlen(input); i++) {
+	for (int i = 0; input[i] != '\0'; i++) {
 		if (input[i] >= 'a' && input[i] <= 'z'
 		||  input[i] >= 'A' && input[i] <= 'Z') {
-			destination[iterator++] = input[i];
+			if (space_needed) {
+				input[iterator++] = ' ';
+				space_needed = false;
+			}
+
+			input[iterator++] = tolower(input[i]);
 			match_found = true;
 		} else if (match_found) {
 			match_found = false;
-			destination[iterator++] = ' ';
+			space_needed = true;
 		}
 	}
 
-	destination[iterator] = '\0';
-	to_lowercase(destination);
+	input[iterator] = '\0';
 }
 
 int main(int argc, const char * argv[]) {
     FILE *file = fopen(argv[1], "r");
-    char line[LINE_MEMORY];
-    char sanitized[LINE_MEMORY];
+    char line[LINE_BUFFER];
 
-    while (fgets(line, LINE_MEMORY, file)) {
-    	sanitize(line, sanitized, LINE_MEMORY);
-        printf("%s\n", sanitized);
+    while (fgets(line, LINE_BUFFER, file)) {
+    	sanitize(line);
+        printf("%s\n", line);
     }
 
     return 0;
