7469da7fea1ffc05287291f9bfe65f9e80c4ae19
Javaliant <luigipvincent@gmail.com>
2016-07-17 15:57:49 -0400

    Add CodeEval Morse Code Challenge Solution in Java.

---
+++ CodeEval/MorseCode/Java/Solution.java
@@ -0,0 +1,82 @@
+/* Author: Luigi Vincent
+*
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+	private static final Map<String, Character> morseAlphabet = new HashMap<>();
+
+	static {
+		morseAlphabet.put(".-", 'A');
+		morseAlphabet.put("-...", 'B');
+		morseAlphabet.put("-.-.", 'C');
+		morseAlphabet.put("-..", 'D');
+		morseAlphabet.put(".", 'E');
+		morseAlphabet.put("..-.", 'F');
+		morseAlphabet.put("--.", 'G');
+		morseAlphabet.put("....", 'H');
+		morseAlphabet.put("..", 'I');
+		morseAlphabet.put(".---", 'J');
+		morseAlphabet.put("-.-", 'K');
+		morseAlphabet.put(".-..", 'L');
+		morseAlphabet.put("--", 'M');
+		morseAlphabet.put("-.", 'N');
+		morseAlphabet.put("---", 'O');
+		morseAlphabet.put(".--.", 'P');
+		morseAlphabet.put("--.-", 'Q');
+		morseAlphabet.put(".-.", 'R');
+		morseAlphabet.put("...", 'S');
+		morseAlphabet.put("-", 'T');
+		morseAlphabet.put("..-", 'U');
+		morseAlphabet.put("...-", 'V');
+		morseAlphabet.put(".--", 'W');
+		morseAlphabet.put("-..-", 'X');
+		morseAlphabet.put("-.--", 'Y');
+		morseAlphabet.put("--..", 'Z');
+		morseAlphabet.put("-----", '0');
+		morseAlphabet.put(".----", '1');
+		morseAlphabet.put("..---", '2');
+		morseAlphabet.put("...--", '3');
+		morseAlphabet.put("....-", '4');
+		morseAlphabet.put(".....", '5');
+		morseAlphabet.put("-....", '6');
+		morseAlphabet.put("--...", '7');
+		morseAlphabet.put("---..", '8');
+		morseAlphabet.put("----.", '9');
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println(args.length > 1 ? "Excessive arguments, only the first will be considered" : "No arguments provided.");
+			System.exit(1);
+		}
+
+		try (Scanner fileScanner = new Scanner(new File(args[0]))) {
+			while (fileScanner.hasNextLine()) {
+				System.out.println(decode(fileScanner.nextLine()));
+			}
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		}
+	}
+
+	private static String decode(String morse) {
+		StringBuilder decoded = new StringBuilder();
+
+		String[] words = morse.split("\\s{2}");
+		for (String word : words) {
+			decoded.append(' ');
+			String[] letters = word.split("\\s");
+			for (String letter : letters) {
+				decoded.append(morseAlphabet.get(letter));
+			}
+		}
+
+		return decoded.substring(1);
+	}
+}
---
+++ CodeEval/MorseCode/README.md
@@ -0,0 +1,23 @@
+###Challenge
+Write a program which reads a file and outputs a specified number of lines, sorted on length in descending order.
+
+###Specifications
+1. The first argument is a path to a file.   
+2. The file contains multiple lines.
+3. Each line is a string of morse code.
+ - Each letter is separated by a space.
+ - Each word is separated by two spaces.
+
+###Constraints
+1. The input file is correctly formatted.
+2. The Morse Code strings are alphanumeric.
+
+**Sample Input**
+>.- ...- ..--- .-- .... .. . -.-. -..-  ....- .....  
+-... .... ...--
+
+**Sample Output**
+>AV2WHIECX 45  
+BH3
+
+[Source](https://www.codeeval.com/open_challenges/116/)
